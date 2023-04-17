cd7c4a0ddeb62fdcf78f8fa444d6906df62cda59
Mohamed Talaat <102768699+MohamedTalaat-Coder@users.noreply.github.com>
2023-04-18 00:17:26 +0200

    create books model

--- src/books/models.py
+++ src/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Books(models.Model):
+    title = models.CharField(max_length=40)
+    author = models.CharField(max_length=40)
+    genre = models.CharField(max_length=25)
+    price = models.IntegerField()
+    rating = models.IntegerField()
+    reviews = models.TextField()
+    buy_option = (
+    ("1", "Cash"),
+    ("2", "Bank Card"),
+    ("3", "PayPal")
+    )
+    date = models.DateField(auto_created=True)
